35aa7e055b5f54a1e51b8421f2d6f2ff3cc48d07
Kenneth Salanga <ksalanga@ucvts.tec.nj.us>
2020-05-29 08:33:17 -0400

    added end boolean to game

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,28 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Game {
     private ArrayList<ChessPiece> whiteCaptures;
     private ArrayList<ChessPiece> blackCaptures;
+    Board board;
+    private boolean end;
 
+    public Game() {
+        //start of the game
+        board = new Board();
+        board.setPositions();
+    }
 
+    public void start() {
+        Scanner s = new Scanner(System.in);
+
+        //put captures ArrayList when a piece moves.
+        while (!end) {
+
+        }
+    }
+
+    public boolean checkmate() {
+        return true;
+    }
 }
