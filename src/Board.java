18955c363917e0e8f15ba28bac7aef1df051298b
Kenneth Salanga <ksalanga@ucvts.tec.nj.us>
2020-05-28 21:39:42 -0400

    added chessboard functionality

--- src/Board.java
+++ src/Board.java
@@ -38,9 +38,27 @@ public class Board {
             Pieces[6][i] = new Pawn(new int[] {1, i}, "white");
         }
 
-        for (int i = 0; i < 8; i++) {
-//            Pieces[0][]
+        ChessPiece[] MajorPieces = new ChessPiece[] {new Rook(null, ""), new Knight(null, ""), new Bishop(null, "")};
+        for (int i = 0; i < 3; i++) {
+            Pieces[0][i] = MajorPieces[i]; //black queens side
+            Pieces[0][i].setColor("black");
+            Pieces[0][i].setPosition(new int[] {0, i});
+            Pieces[0][7 - i] = MajorPieces[i]; //black kings side
+            Pieces[0][7 - i].setColor("black");
+            Pieces[0][7 - i].setPosition(new int[] {0, 7 - i});
+            Pieces[7][i] = MajorPieces[i]; //white queens side
+            Pieces[7][i].setColor("white");
+            Pieces[7][i].setPosition(new int[] {7, i});
+            Pieces[7][7 - i] = MajorPieces[i]; //white kings side
+            Pieces[7][7 - i].setColor("white");
+            Pieces[7][7 - i].setPosition(new int[] {7, 7 - i});
         }
+
+        //King and Queens
+        Pieces[0][3] = new Queen(new int[] {0, 3}, "black");
+        Pieces[7][3] = new Queen(new int[] {7, 3}, "white");
+        Pieces[0][4] = new King(new int[] {0, 4}, "black");
+        Pieces[7][4] = new King(new int[] {7, 4}, "white");
     }
 
     public int[] convertToCoords(String tile) {
