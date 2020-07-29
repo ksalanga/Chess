dc70937b649097b3b681a8b1c7e72d6e335c23ad
Kenneth Salanga <ksalanga@ucvts.tec.nj.us>
2020-07-28 21:13:44 -0400

    added actionListener to Board

--- src/GUI.java
+++ src/GUI.java
@@ -1,43 +1,36 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.Color;
-import java.awt.Grid;
+import java.awt.GridLayout;
 
-public class GUI extends JPanel implements MouseListener {
+import static com.sun.java.accessibility.util.AWTEventMonitor.addActionListener;
 
-    public static void main(String args[]) {
+public class GUI extends JPanel implements MouseListener, ActionListener {
 
+    public GUI() {
         JPanel jp = new JPanel();
         jp.setSize(800,800);
         jp.setLayout(new GridLayout(8,8));
 
         JFrame f = new JFrame();//creating instance of JFrame
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                JButton b = new JButton();
-                if ((i+j)%2==0) b.setBackground(Color.white);
-                else b.setBackground(Color.black);
-                jp.add(b);
-
-            }
-        }
-        //DOT setbounds can do this for JPanel.
-        
-
-        //if we have box we dont have to do the x y for this.
-
         f.add(jp);//adding button in JFrame
 
         f.setSize(1920,1080);//400 width and 500 height
         f.setLayout(null);//using no layout managers
         f.setVisible(true);//making the frame visible
 
-    }
-
-    public GUI() {
-        addMouseListener(this);
-
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                JButton b = new JButton();
+                if ((i+j)%2==0) b.setBackground(Color.white);
+                else b.setBackground(Color.black);
+                jp.add(b);
+                b.addActionListener(this);
+            }
+        }
     }
 
     private void addMouseListener(GUI gui) {
@@ -45,26 +38,31 @@ public class GUI extends JPanel implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
-
+        System.out.println("Click");
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-
+        System.out.println("Click1");
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-
+        System.out.println("Click2");
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
-
+        System.out.println("Click3");
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
+        System.out.println("Click4");
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Clicked");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,47 +2,48 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Game g = new Game();
-
-        String[] shortestStalemate = new String[]{"e2", "e3",
-                "a7", "a5",
-                "d1", "h5",
-                "a8", "a6",
-                "h5", "a5",
-                "h7", "h5",
-                "h2", "h4",
-                "a6", "h6",
-                "a5", "c7",
-                "f7", "f6",
-                "c7", "d7",
-                "e8", "f7",
-                "d7", "b7",
-                "d8", "d3",
-                "b7", "b8",
-                "d3", "h7",
-                "b8", "c8",
-                "f7", "g6",
-                "c8", "e6"};
-
-        String[] checkMate = new String[] {
-                "e2", "e4",
-                "a7", "a6",
-                "f1", "c4",
-                "b7", "b6",
-                "d1", "f3",
-                "c7", "c6",
-                "f3", "f7"
-        }; //checkmate Works
-
-        String[] rookDissappears = new String[] {
-                "a2", "a4",
-                "a7", "a5",
-                "a1", "a3",
-                "b7", "b5",
-                "a3", "c3",
-                "b8", "a6",
-                "c3", "c7"
-        };
-        g.start();
+        GUI g = new GUI();
+//        Game g = new Game();
+//
+//        String[] shortestStalemate = new String[]{"e2", "e3",
+//                "a7", "a5",
+//                "d1", "h5",
+//                "a8", "a6",
+//                "h5", "a5",
+//                "h7", "h5",
+//                "h2", "h4",
+//                "a6", "h6",
+//                "a5", "c7",
+//                "f7", "f6",
+//                "c7", "d7",
+//                "e8", "f7",
+//                "d7", "b7",
+//                "d8", "d3",
+//                "b7", "b8",
+//                "d3", "h7",
+//                "b8", "c8",
+//                "f7", "g6",
+//                "c8", "e6"};
+//
+//        String[] checkMate = new String[] {
+//                "e2", "e4",
+//                "a7", "a6",
+//                "f1", "c4",
+//                "b7", "b6",
+//                "d1", "f3",
+//                "c7", "c6",
+//                "f3", "f7"
+//        }; //checkmate Works
+//
+//        String[] rookDissappears = new String[] {
+//                "a2", "a4",
+//                "a7", "a5",
+//                "a1", "a3",
+//                "b7", "b5",
+//                "a3", "c3",
+//                "b8", "a6",
+//                "c3", "c7"
+//        };
+//        g.start();
     }
 }
