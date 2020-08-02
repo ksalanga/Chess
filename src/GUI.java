3f49b592d86254bccb32643fbd8aa50ac872cf09
Kenneth Salanga <ksalanga@ucvts.tec.nj.us>
2020-08-02 19:23:49 -0400

    deleted Tile class as it was unneccessary. Loaded in all pieces into the GUI, working on moving

--- src/GUI.java
+++ src/GUI.java
@@ -1,38 +1,92 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Color;
-import java.awt.GridLayout;
 
 public class GUI extends JPanel implements ActionListener {
 
-    private Tile[][] t;
-
     public GUI() {
+
         Board board = new Board();
+        board.setPositions();
 
-        Tile[][] t = new Tile[8][8];
-
-        JPanel jp = new JPanel();
-        jp.setSize(800,800);
-        jp.setLayout(new GridLayout(8,8));
+//        ImageIcon knight = new ImageIcon("images/BlackKnight.png");
+//        Image img = knight.getImage();
+//
+//        Image newimg = img.getScaledInstance(70,70,0);
+        JPanel GUIboard = new JPanel();
+        GUIboard.setSize(800,800);
+        GUIboard.setLayout(new GridLayout(8,8));
 
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                t[i][j] = new Tile(Board.getPieces()[i][j]);
                 JButton b = new JButton();
-                if ((i+j)%2==0) b.setBackground(Color.white);
-                else b.setBackground(Color.black);
-                jp.add(b);
+
+                if(!(Board.getPieces()[i][j] == null)) {
+                    String s = "";
+                    if (Board.getPieces()[i][j].getColor().equals("white")) {
+                        switch(Board.getPieces()[i][j].getName()) {
+                            case "♔":
+                                s = "images/WhiteKing.png";
+                                break;
+                            case "♕":
+                                s = "images/WhiteQueen.png";
+                                break;
+                            case "♖":
+                                s = "images/WhiteRook.png";
+                                break;
+                            case "♗":
+                                s = "images/WhiteBishop.png";
+                                break;
+                            case "♘":
+                                s = "images/WhiteKnight.png";
+                                break;
+                            case "♙":
+                                s = "images/WhitePawn.png";
+                                break;
+                        }
+                    } else {
+                        switch (Board.getPieces()[i][j].getName()) {
+                            case "♚":
+                                s = "images/BlackKing.png";
+                                break;
+                            case "♛":
+                                s = "images/BlackQueen.png";
+                                break;
+                            case "♜":
+                                s = "images/BlackRook.png";
+                                break;
+                            case "♝":
+                                s = "images/BlackBishop.png";
+                                break;
+                            case "♞":
+                                s = "images/BlackKnight.png";
+                                break;
+                            case "♟":
+                                s = "images/BlackPawn.png";
+                                break;
+                        }
+                    }
+                    b.setIcon(new ImageIcon(s));
+                }
+                if ((i+j)%2==0) b.setBackground(new Color(225, 197, 158));
+                else b.setBackground(new Color(101, 48, 17));
+                GUIboard.add(b);
                 b.addActionListener(this);
 //                b.setRolloverIcon(); //for hovering over the buttons
 //                b.setPressedIcon();
             }
         }
 
+//        JButton button = new JButton();
+//        button.setBounds(0,0,70,70);
+//        button.setIcon(new ImageIcon(newimg));
+
 
         JFrame f = new JFrame();//creating instance of JFrame
-        f.add(jp);//adding button in JFrame
+        f.add(GUIboard);//adding button in JFrame
+//        f.add(button);
+//        button.setBackground(new Color(225, 197, 158));
 
         f.setSize(1920,1080);//400 width and 500 height
         f.setLayout(null);//using no layout managers
